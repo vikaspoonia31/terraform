44a92f19392e15ec13fde71afa777aed0079a4bd
Vikas Poonia <Vikas.Poonia@gds.ey.com>
2022-04-21 11:10:58 +0530

    3rd Testing

--- applications.tf
+++ applications.tf
@@ -9,3 +9,15 @@ resource "auth0_client" "terraform-secure-express" {
     alg = "RS256"
   }
 }
+
+resource "auth0_client" "terraform-secure-express" {
+  name            = "My Web Demo App 2"
+  description     = "App for running testing 2"
+  app_type        = "regular_web"
+  callbacks       = ["http://localhost:3000/callback"]
+  oidc_conformant = true
+
+  jwt_configuration {
+    alg = "RS256"
+  }
+}
